842754065aab0bfa0fab89cc57f431b823758bb4
Prasenjit <prasenjitbhosale1234@gmail.com>
2023-05-03 15:58:20 +0530

    first commit

---
+++ frontend/src/components/Image-gallery/galleryImage.js
@@ -0,0 +1,22 @@
+import galleryImg01 from '../../assets/images/yjhd.jpg'
+import galleryImg02 from '../../assets/images/monk.jpg'
+import galleryImg03 from '../../assets/images/rajastan.jpg'
+import galleryImg04 from '../../assets/images/gallery-04.jpg'
+import galleryImg05 from '../../assets/images/gallery-05.jpg'
+import galleryImg06 from '../../assets/images/surf.jpg'
+import galleryImg07 from '../../assets/images/buddha1.jpg'
+import galleryImg08 from '../../assets/images/gallery-02.jpg'
+
+
+const galleryImages = [
+   galleryImg01,
+   galleryImg06,
+   galleryImg04,
+   galleryImg03,
+   galleryImg08,
+   galleryImg02,
+   galleryImg07,
+   galleryImg05,
+]
+
+export default galleryImages
